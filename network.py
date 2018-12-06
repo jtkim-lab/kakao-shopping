d67c0ea129c45f393cf7eba84bcea5b0d28ee5ad
Jungtaek Kim <jtkim@postech.ac.kr>
2018-12-06 21:02:42 +0900

    UPDATE: model

--- network.py
+++ network.py
@@ -7,6 +7,9 @@ opt = Option('./config.json')
 
 acc = tf.metrics.accuracy
 dense = tf.layers.dense
+conv = tf.layers.conv1d
+pool = tf.layers.max_pooling1d
+flatten = tf.layers.flatten
 dropout = tf.layers.dropout
 bn = tf.layers.batch_normalization
 relu = tf.nn.relu
@@ -17,7 +20,7 @@ class Model(object):
     def __init__(self):
         self.logger = get_logger('Model')
 
-    def get_model(self, num_classes, activation=relu):
+    def get_model(self, num_classes, activation=tanh):
         len_max = opt.max_len
         size_voca = opt.unigram_hash_size + 1
 
@@ -32,24 +35,36 @@ class Model(object):
         outs_w = tf.expand_dims(w_uni, axis=1)
         
         rate_dropout = 0.4
-#        bias_1 = tf.get_variable('bias_1', shape=(1, opt.size_embedding), dtype=tf.float32)
-        outs = tf.matmul(outs_w, outs)
+        bias_1 = tf.get_variable('bias_1', shape=(1, opt.size_embedding), dtype=tf.float32)
+        outs = tf.matmul(outs_w, outs) + bias_1
         outs = tf.squeeze(outs, axis=1)
-        outs = dense(outs, 128)
-        outs = dropout(outs, rate=rate_dropout, training=is_training)
+
+#        outs = tf.expand_dims(outs, axis=2)
+
+#        outs = conv(outs, 32, 3, activation=activation, padding='same')
+#        outs = pool(outs, 2, 2)
 #        outs = bn(outs, training=is_training)
-        outs = activation(outs)
+
+#        outs = conv(outs, 64, 3, activation=activation, padding='same')
+#        outs = pool(outs, 2, 2)
+#        outs = bn(outs, training=is_training)
+
+#        outs = flatten(outs)
+
         outs = dense(outs, 256)
-        outs = dropout(outs, rate=rate_dropout, training=is_training)
-#        outs = bn(outs, training=is_training)
+#        outs = dropout(outs, rate=rate_dropout, training=is_training)
         outs = activation(outs)
+        outs = bn(outs, training=is_training)
+
         outs = dense(outs, 512)
-        outs = dropout(outs, rate=rate_dropout, training=is_training)
-#        outs = bn(outs, training=is_training)
+#        outs = dropout(outs, rate=rate_dropout, training=is_training)
         outs = activation(outs)
-#        outs = dense(outs, 256)
-#        outs = relu(outs)
-#        outs = bn(outs, training=is_training)
+        outs = bn(outs, training=is_training)
+
+        outs = dense(outs, 1024)
+#        outs = dropout(outs, rate=rate_dropout, training=is_training)
+        outs = activation(outs)
+        outs = bn(outs, training=is_training)
 
         outs = dense(outs, num_classes)
 
@@ -58,7 +73,10 @@ class Model(object):
         loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=targets, logits=outs)
         loss = tf.reduce_mean(loss)
         opt_adam = tf.train.AdamOptimizer(learning_rate)
-        optimizer = opt_adam.minimize(loss)
+
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            optimizer = opt_adam.minimize(loss)
 
         model = {
             'uni': uni,
